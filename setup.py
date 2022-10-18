d23cf3a03e39d4d906092b6c5ff2c9a0dc1ecb38
Sali273 <sali274@hotmail.fr>
2022-10-18 23:31:10 +0200

    ajout de setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import setuptools import setup, find_packages
+
+setup(
+    name = "churn",
+    packages = find_packages(),
+    description = "Classification de l'attrition d'un client d'une agence bancaire "
+)
