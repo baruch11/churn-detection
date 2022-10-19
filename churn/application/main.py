35f9ac89a8bab6216c0a6e5ff9d639f681f9b1da
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-19 19:17:04 +0200

    Revert "ChurnModel abstraction created"
    This reverts commit c6bb9cd9fe3740dc052863e749634eea23ad11ce.

--- churn/application/main.py
+++ churn/application/main.py
@@ -3,28 +3,21 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-from churn.domain.churn_model import DummyChurnModel
 from churn.infrastructure.bank_customers import BankCustomersData
 from churn.domain.bank_customers_dataset import FeaturesDataset
-from churn.config.config import read_yaml
 
 np.random.seed(42)
 
-CONFIG = read_yaml("churn/config/config_template.yml")
 
-CONFIG_DATA_INDICATORS = CONFIG["data"]["indicators_dataset"]
-CONFIG_DATA_CUSTOMERS = CONFIG["data"]["customers_dataset"]
-bcd = BankCustomersData(CONFIG_DATA_INDICATORS, CONFIG_DATA_CUSTOMERS)
+F_INDICATORS = "./data/1_-_indicators.csv"
+F_CUSTOMERS = "./data/1_-_customers.csv"
+bcd = BankCustomersData(F_INDICATORS, F_CUSTOMERS)
 raw_data = bcd.load_data()
 
 # feature engineering
 fds = FeaturesDataset(balance_imputation="drop")
 features = fds.compute_features(raw_data)
-dummy = DummyChurnModel(X=features.drop(columns=["CHURN"]),y=features["CHURN"])
-print("dummy fiting :",dummy.fit())
-print("dummy predicting :",dummy.predict())
-print("dummy loading :",dummy.load())
-print("dummy saving :",dummy.save())
+
 # model performance
 clf = DecisionTreeClassifier(max_depth=5)
 X_train, X_test, y_train, y_test = train_test_split(
--- churn/config/config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-def read_yaml(path: str) -> dict:
-    """Returns YAML file as dict"""
-    with open(path, 'r') as file_in:
-        config = yaml.safe_load(file_in)
-    return config
-
