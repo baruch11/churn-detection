3bb6a0b3562a436496f289e39a7b22b788ab111b
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-20 20:05:00 +0200

    added possibility to save and load ChurnModel Class

--- churn/domain/churn_model.py
+++ churn/domain/churn_model.py
@@ -2,8 +2,7 @@ from abc import ABC, ABCMeta, abstractmethod
 from xmlrpc.client import Boolean
 import pandas as pd
 from sklearn.pipeline import Pipeline
-
-
+import pickle
 
 class BaseChurnModel(metaclass=ABCMeta):
     """
@@ -14,7 +13,6 @@ class BaseChurnModel(metaclass=ABCMeta):
     @abstractmethod
     def fit(self):
         raise NotImplementedError
-
     @abstractmethod
     def predict(self):
         raise NotImplementedError
@@ -26,16 +24,39 @@ class BaseChurnModel(metaclass=ABCMeta):
         raise NotImplementedError
 
 class DummyChurnModel(BaseChurnModel):
-    def __init__(self,X : pd.DataFrame = None, y :pd.DataFrame = None, pipeline : Pipeline = None, params : dict = None) :
-        self.X = X 
-        self.y = y 
-        self.pipeline = pipeline
-        self.params = dict or {}
-    def fit(self) -> 'DummyChurnModel' :
+    PICKLE_PATH ="churn/config/DummyChurnModel.pkl"
+    def __init__(self,params : dict = None) :
+        self.params = params or {}
+
+    def fit(self,X : pd.DataFrame, y :pd.DataFrame, pipeline : Pipeline) -> 'DummyChurnModel' :
+        """Build the models of the given pipeline from the training set (X, y).
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            The training input samples. 
+
+        y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+            The target values (class labels) as integers or strings.
+
+        pipeline : 
+            Pipeline of transforms with a final estimator.
+
+        Returns
+        -------
+        self : DummyChurnModel
+            Fitted pipeline.
+        """
+        self.pipeline = pipeline.fit(X, y)
         return self
-    def predict(self):
-        return self.y
-    def load(self) -> Boolean:
-        return True
+    def predict(self,X : pd.DataFrame):
+        predict =self.pipeline.predict(X)
+        return predict
+    @staticmethod
+    def load() -> Boolean:
+        with open(str(DummyChurnModel.PICKLE_PATH), 'rb') as inp:
+            return pickle.load(inp)
     def save(self) -> Boolean:
+        with open(str(self.PICKLE_PATH), 'wb') as outp:
+            pickle.dump(self, outp, pickle.HIGHEST_PROTOCOL)
         return True 
