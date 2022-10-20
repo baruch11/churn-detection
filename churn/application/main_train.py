d10572f6d6b976f5c86d95eadbf88ff8128a2c04
Sali273 <sali274@hotmail.fr>
2022-10-20 06:03:47 +0200

    add main_train.py

---
+++ churn/application/main_train.py
@@ -0,0 +1,29 @@
+"""Compute training from a csv file"""
+import argparse
+#from abc import ABC, ABCMeta, abstractmethod
+from churn.domain.churn_model import DummyChurnModel
+from churn.infrastructure.bank_customers import BankCustomersData
+
+
+PARSER = argparse.ArgumentParser(
+    description='Compute training from a csv file')
+PARSER.add_argument('--indicators_csv', '-i', type=str,
+                    help='indicators csv file name')
+PARSER.add_argument('--customers_csv', '-c', type=str,
+                    help='customers csv file name')
+PARSER.add_argument('--output_csv', '-o', type=str,
+                    help='output training csv file name')
+
+
+args = PARSER.parse_args()
+
+#instanciation des données à partir de la classe BankCustomersData
+#du module /infrastructure/bank_customers.py
+bcd = BankCustomersData(args.indicators_csv, args.customers_csv)
+raw_data = bcd.load_data()
+
+#training du modèle à partir de la classe DummyChurnModel
+# du module /domain/churn_model.py
+model = DummyChurnModel().load()
+parameters = model.fit(raw_data)
+parameters.to_csv(args.output_csv)
