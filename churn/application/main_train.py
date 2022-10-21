3f3dbd503bc282dd0770698b7786e9d540a3ac81
Sali273 <sali274@hotmail.fr>
2022-10-21 21:51:31 +0200

    split raw_data with sklearn train_test_split

--- churn/application/main_train.py
+++ churn/application/main_train.py
@@ -1,7 +1,7 @@
 """Compute training from a csv file"""
 
 import argparse
-#from abc import ABC, ABCMeta, abstractmethod
+from sklearn.model_selection import train_test_split
 from churn.domain.churn_model import DummyChurnModel
 from churn.infrastructure.bank_customers import BankCustomersData
 
@@ -21,8 +21,11 @@ args = PARSER.parse_args()
 bcd = BankCustomersData(args.indicators_csv, args.customers_csv)
 raw_data = bcd.load_data()
 
+X_train, X_test, y_train, y_test = train_test_split(raw_data.drop(columns=["CHURN"]), raw_data["CHURN"], test_size=0.20, random_state=33)
+
+
 #training du modèle à partir de la classe DummyChurnModel
 # du module /domain/churn_model.py
 model = DummyChurnModel()
-model.fit(raw_data.drop(columns=["CHURN"]), raw_data["CHURN"]))
+model.fit(X_train, y_train)
 model.save()
