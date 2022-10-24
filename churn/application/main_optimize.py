2a7ae1ae4d922a80e6d0e1610feda507b824bde6
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-24 21:58:53 +0200

    Model mapping trick to load class from file

--- churn/application/main_optimize.py
+++ churn/application/main_optimize.py
@@ -1,9 +1,6 @@
-"""Main for churn modelling """
-from curses import raw
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import svm, datasets
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 from sklearn.utils.estimator_checks import check_estimator
@@ -16,21 +13,28 @@ from churn.domain.bank_customers_dataset import FeaturesDataset
 from churn.config.config import read_yaml
 
 np.random.seed(42)
-
+MODELS_MAPPING_DICT={
+    "FeatureDataset" : FeaturesDataset(),
+    "SVC()" : SVC(),
+    "DecisionTreeClassifier()" : DecisionTreeClassifier()
+}
 CONFIG = read_yaml("churn/config/config_template.yml")
 
 CONFIG_DATA_INDICATORS = CONFIG["data"]["indicators_dataset"]
 CONFIG_DATA_CUSTOMERS = CONFIG["data"]["customers_dataset"]
+CONFIG_MODEL_PARAMETERS = CONFIG["model"]["grid_search_params"]
+first_estimator = CONFIG_MODEL_PARAMETERS[0]["pipeline__classifier"][0] = MODELS_MAPPING_DICT[CONFIG_MODEL_PARAMETERS[0]["pipeline__classifier"][0]]
+first_grid_params = CONFIG_MODEL_PARAMETERS[0]
+#CONFIG_MODEL_PARAMETERS[1]["pipeline__classifier"][0] = MODELS_MAPPING_DICT[CONFIG_MODEL_PARAMETERS[1]["pipeline__classifier"][0]]
+
+
+
 bcd = BankCustomersData(CONFIG_DATA_INDICATORS, CONFIG_DATA_CUSTOMERS)
 raw_data = bcd.load_data()
-raw_data = raw_data[0:100]
-#TODO : Faire une feature request pour eque l'objet FeatureDataset puisse fonctionner sur X et sur y, sinon on ne pourra jamais faire un ChurnModel.fit(feature.drop('CHURN'),feature["CHURN"])
-fds = FeaturesDataset(balance_imputation="drop")
-features = fds.compute_features(raw_data)
-# feature engineering
-estimator = ChurnModelSelection(pipeline=Pipeline([('scaler', StandardScaler()), ('svc', SVC())]))
-#TODO : Faire varier balance imputation
-params={'pipeline__svc__C':[.01,.05,.1,.5,1,5,10]}
-clf = GridSearchCV(estimator, params)
-clf.fit(X=features.drop(columns=["CHURN"]),y=features["CHURN"])
-print(clf.cv_results_)
+
+X_train, X_test, y_train, y_test = train_test_split(raw_data.drop(columns=["CHURN"]), raw_data["CHURN"], test_size=0.20, random_state=33)
+model = ChurnModelSelection(pipeline=Pipeline([('features', FeaturesDataset()), ('classifier', first_estimator)]))
+clf = GridSearchCV(model, first_grid_params)
+clf.fit(X_train,y_train)
+print(clf.best_params_)
+print(clf.score(X_test,y_test))
