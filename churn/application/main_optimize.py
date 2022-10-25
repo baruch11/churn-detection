ee6b0cd40bc7ae7dfaef8a833629a1af8a970ef8
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-25 12:32:55 +0200

    Modified config reading of models parameters

--- churn/application/main_optimize.py
+++ churn/application/main_optimize.py
@@ -7,31 +7,33 @@ from sklearn.utils.estimator_checks import check_estimator
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
 from churn.domain.churn_model import ChurnModelSelection
 from churn.infrastructure.bank_customers import BankCustomersData
 from churn.domain.bank_customers_dataset import FeaturesDataset
-from churn.config.config import read_yaml
+from churn.config.config import read_yaml, transform_to_object
 
 np.random.seed(42)
 MODELS_MAPPING_DICT={
-    "FeatureDataset" : FeaturesDataset(),
     "SVC()" : SVC(),
-    "DecisionTreeClassifier()" : DecisionTreeClassifier()
+    "DecisionTreeClassifier()" : DecisionTreeClassifier(),
+    "RandomForestClassifier()" : RandomForestClassifier(),
+    "AdaBoostClassifier()" : AdaBoostClassifier(),
+    "MLPClassifier()" : MLPClassifier()
 }
 CONFIG = read_yaml("churn/config/config_template.yml")
 
 CONFIG_DATA_INDICATORS = CONFIG["data"]["indicators_dataset"]
 CONFIG_DATA_CUSTOMERS = CONFIG["data"]["customers_dataset"]
-CONFIG_MODEL_PARAMETERS = CONFIG["model"]["grid_search_params"]
-first_estimator = CONFIG_MODEL_PARAMETERS[0]["pipeline__classifier"][0] = MODELS_MAPPING_DICT[CONFIG_MODEL_PARAMETERS[0]["pipeline__classifier"][0]]
-first_grid_params = CONFIG_MODEL_PARAMETERS[0]
-#CONFIG_MODEL_PARAMETERS[1]["pipeline__classifier"][0] = MODELS_MAPPING_DICT[CONFIG_MODEL_PARAMETERS[1]["pipeline__classifier"][0]]
+models = transform_to_object("grid_search_params",MODELS_MAPPING_DICT)
+#MAPPING_DICT[CONFIG_MODEL_PARAMETERS[1]["pipeline__classifier"][0]]
 
-
+print(type(models[0]["pipeline__classifier"][0]))
 
 bcd = BankCustomersData(CONFIG_DATA_INDICATORS, CONFIG_DATA_CUSTOMERS)
 raw_data = bcd.load_data()
-
+# TODO : Faire un for pour boucler sur chaque modèle, et exporter les résultats _best_params, _best_score pour chacun d'eux. 
 X_train, X_test, y_train, y_test = train_test_split(raw_data.drop(columns=["CHURN"]), raw_data["CHURN"], test_size=0.20, random_state=33)
 model = ChurnModelSelection(pipeline=Pipeline([('features', FeaturesDataset()), ('classifier', first_estimator)]))
 clf = GridSearchCV(model, first_grid_params)
--- churn/config/config.py
+++ churn/config/config.py
@@ -1,3 +1,5 @@
+
+from typing import Dict
 import yaml
 
 def read_yaml(path: str) -> dict:
@@ -5,4 +7,13 @@ def read_yaml(path: str) -> dict:
     with open(path, 'r') as file_in:
         config = yaml.safe_load(file_in)
     return config
+
+def transform_to_object(path_of_models : str ,mapping_dict : Dict):
+    config = read_yaml("churn/config/config_template.yml")
+    grid_parameters = config["model"][str(path_of_models)]
+    model_list = list()
+    for model in grid_parameters :
+        model["pipeline__classifier"][0] = mapping_dict[model["pipeline__classifier"][0]]
+        model_list.append(model)
+    return model_list
 
