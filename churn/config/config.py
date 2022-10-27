751e57b12604304df58e25e14ac8ac77f598952e
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-27 13:55:26 +0200

    Allow tuple format in yaml + doc

--- churn/config/config.py
+++ churn/config/config.py
@@ -1,14 +1,21 @@
 
+from calendar import c
 from typing import Dict
 import yaml
 
 def read_yaml(path: str) -> dict:
     """Returns YAML file as dict"""
     with open(path, 'r') as file_in:
-        config = yaml.safe_load(file_in)
+        config = yaml.unsafe_load(file_in)
     return config
 
+
+    return 
 def transform_to_object(path_of_models : str ,mapping_dict : Dict):
+    """
+    This function transform models name from string to class. 
+    Ex : from "SVC()" to sklearn.SVC() """
+
     config = read_yaml("churn/config/config_template.yml")
     grid_parameters = config["model"][str(path_of_models)]
     model_list = list()
