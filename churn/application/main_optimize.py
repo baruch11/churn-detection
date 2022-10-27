57eee93821aaa7fbb62418ea8c0e4ad3c1191e41
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-27 16:32:24 +0200

    Added some models and their params to play

--- churn/application/main_optimize.py
+++ churn/application/main_optimize.py
@@ -9,33 +9,34 @@ from sklearn.utils.estimator_checks import check_estimator
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
+from interpret.glassbox import ExplainableBoostingClassifier
 from skopt import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
 from churn.domain.churn_model import ChurnModelSelection
-from churn.domain.domain_utils import get_train_test_split
+from churn.domain.domain_utils import get_train_test_split, return_models_from_all_model_params, find_model_params_from_model_name
 from churn.domain.bank_customers_dataset import FeaturesDataset
 from churn.config.config import read_yaml, transform_to_object
-
+from churn.domain.domain_utils import get_train_test_split
+from churn.infrastructure.bank_customers import BankCustomersData
 np.random.seed(42)
 MODELS_MAPPING_DICT={
     "SVC()" : SVC(),
     "DecisionTreeClassifier()" : DecisionTreeClassifier(),
     "RandomForestClassifier()" : RandomForestClassifier(),
-    "AdaBoostClassifier()" : AdaBoostClassifier(),
-    "MLPClassifier()" : MLPClassifier()
+    "MLPClassifier()" : MLPClassifier(),
+    "GradientBoostingClassifier()" : GradientBoostingClassifier(),
+    "ExplainableBoostingClassifier()" : ExplainableBoostingClassifier()
 }
 CONFIG = read_yaml("churn/config/config_template.yml")
 
 CONFIG_DATA_INDICATORS = CONFIG["data"]["indicators_dataset"]
 CONFIG_DATA_CUSTOMERS = CONFIG["data"]["customers_dataset"]
 all_models_param = transform_to_object("grid_search_params",MODELS_MAPPING_DICT)
+all_models = return_models_from_all_model_params(all_models_param)
 
-all_models = list()
-# Function list_all_models, to create in param
-for model in all_models_param:
-    all_models.append(model['pipeline__classifier'][0])
+
 
 
 parser = argparse.ArgumentParser()
@@ -44,18 +45,26 @@ parser.add_argument("-m", "--models",
                 action="store_true")
 parser.add_argument("-g", "--GridSearchModel", type=str,
                     help=("Compute a gridSearch for a specific model with all parameters defined in the config file."))
+parser.add_argument("-n", "--n_iter", type=str,
+                    help=("(Work only for GridSearchModel) : Number of parameter settings that are sampled. n_iter trades off runtime vs quality of the solution. Default is 5"),
+                    default=5)
 args = parser.parse_args()
+
 
 bcd = BankCustomersData(CONFIG_DATA_INDICATORS, CONFIG_DATA_CUSTOMERS)
 raw_data = bcd.load_data()
 feature = FeaturesDataset().transform(raw_data)
+# TODO : Mettre les raw data dans le pipeline
+# TODO : Décommenter les lignes du fichier de config
+# TODO : Enlever le test size = 0.95
 #Waiting for the solution @Charles.
-X_train, X_test, y_train, y_test = train_test_split(feature.drop(columns=["CHURN"]), feature["CHURN"], test_size=0.95, random_state=33)
-#X_train, X_test, y_train, y_test = train_test_split(raw_data.drop(columns=["CHURN"]), raw_data["CHURN"], test_size=0.80, random_state=33)
+X_train, X_test, y_train, y_test = train_test_split(feature.drop(columns=["CHURN"]), feature["CHURN"], test_size=0.20, random_state=33)
+#X_train, X_test, y_train, y_test = get_train_test_split()
+
 print(type(all_models_param[0]["pipeline__classifier__gamma"][0]))
-#all_models_param[0]["pipeline__classifier__gamma"] = (1e-6, 1e+1, 'log-uniform')
 print(all_models_param[0]["pipeline__classifier__gamma"])
-#Si l'utilisateur veut visualiser les scores de chacun des modèles avec leurs paramètres par défaut. 
+
+#If the user want to visualize the score of each one of the different models. 
 if args.models:
     for model_classifier in all_models:
         print(f"Modèle en cours d'entrainement : {model_classifier}")
@@ -65,24 +74,20 @@ if args.models:
         clf.fit(X_train,y_train)
         print(clf.best_score_)
 
-    
+#If the user want to commpute a Bayesian Search Optimization for a specific model
 if args.GridSearchModel:
-    #Mettre ça dans un utils
-    for model_parameters in all_models_param:
-        classifier = model_parameters["pipeline__classifier"][0]
-        if f"{classifier.__class__.__name__}()" == args.GridSearchModel :
-            print("Run this model, this is the right one")
-            #TODO : Remettre en place l'appel à FeaturesDataset.             
-            model = ChurnModelSelection(pipeline=Pipeline([ ('scaler', StandardScaler()),('classifier', classifier)]))
-            opt = BayesSearchCV(
-                model,
-                [(model_parameters, 5)],
-                #Change cv to 5
-                cv=2 )
-            opt.fit(X_train, y_train)
-            print("val. score: %s" % opt.best_score_)
-            print("test score: %s" % opt.score(X_test, y_test))
-            print(f"best params : {opt.best_params_}")
+    model_parameters = find_model_params_from_model_name(all_models_param,model_name=args.GridSearchModel)
+    classifier = model_parameters["pipeline__classifier"][0]      
+    model = ChurnModelSelection(pipeline=Pipeline([ ('scaler', StandardScaler()),('classifier', classifier)]))
+    
+    opt = BayesSearchCV(
+        model,
+        [(model_parameters, int(args.n_iter))],
+        cv=5 )
+    opt.fit(X_train, y_train)
+    print("val. score: %s" % opt.best_score_)
+    print("test score: %s" % opt.score(X_test, y_test))
+    print(f"best params : {opt.best_params_}")
 
 exit()
 
