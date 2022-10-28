134a8dfc87ac0c62e6e5df46b4ab58ea2c164deb
chp <charprat@yahoo.fr>
2022-10-28 16:02:26 +0200

    minor changes : cosmetics and documentation

--- churn/domain/bank_customers_dataset.py
+++ churn/domain/bank_customers_dataset.py
@@ -5,10 +5,11 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.base import TransformerMixin
 from sklearn.base import BaseEstimator
 
+
 @dataclass
 class FeaturesDataset(TransformerMixin, BaseEstimator):
     """This class represents the features of the churn modelling."""
-   
+
     features: pd.DataFrame = None
     balance_imputation: str = "median"
     imput_nan_salaire = None
@@ -18,21 +19,26 @@ class FeaturesDataset(TransformerMixin, BaseEstimator):
     imput_zero_balance = None
     land_encoder = OneHotEncoder(handle_unknown='ignore')
 
-
-    def fit(self, X:pd.DataFrame, y=None):
-        
+    def fit(self, X: pd.DataFrame, y=None):
+        """Fit the feature engineering transformer.
+        Parameters
+        ----------
+        X (pd.DataFrame): raw data output of
+             infrastructure/BankCustomersData.load_data()
+        y: not used, sklearn API compatibility
+        """
         assert self.balance_imputation in {"median", "mean", "none"}
-        
+
         zeros_pos = X["BALANCE"] == 0
         outliers_pos = X["AGE"] > 100
         self.imput_nan_salaire = X["SALAIRE"].median()
         self.imput_nan_score_credit = X["SCORE_CREDIT"].median()
         self.imput_outliers_age = X.loc[~outliers_pos, "AGE"].median()
         self.imput_nan_balance = X["BALANCE"].median()
-        
+
         if self.balance_imputation == "median":
             self.imput_zero_balance = X.loc[~zeros_pos, "BALANCE"].median()
-        
+
         if self.balance_imputation == "mean":
             self.imput_zero_balance = X.loc[~zeros_pos, "BALANCE"].mean()
 
@@ -40,36 +46,42 @@ class FeaturesDataset(TransformerMixin, BaseEstimator):
 
         return self
 
-    def transform(self, X_processed:pd.DataFrame, y=None) -> pd.DataFrame:
+    def transform(self, raw_data: pd.DataFrame, y=None) -> pd.DataFrame:
         """Compute features for churn detection.
-        Args:
-            raw_data (pd.DataFrame): output of BankCustomersData.load_data()
+        Parameters
+        ----------
+        raw_data (pd.DataFrame): output of BankCustomersData.load_data()
+        y: not used, sklearn API compatibility
+
+        Returns
+        -------
+        features (pd.DataFrame): features matrix
         """
 
         assert self.balance_imputation in {"median", "mean", "none"}
-       
-        self.features = X_processed.drop(columns=["NOM"])\
-                                .assign(NUM_DAYS=X_processed.DATE_ENTREE.apply(
-                                    lambda x: (X_processed.DATE_ENTREE.max()-x).days))\
-                                .assign(DAY_OF_YEAR=X_processed.DATE_ENTREE.dt.dayofyear)\
-                                .drop(columns=["DATE_ENTREE"])
-        
+
+        self.features = raw_data.drop(columns=["NOM"])\
+            .assign(NUM_DAYS=raw_data.DATE_ENTREE.apply(
+                lambda x: (raw_data.DATE_ENTREE.max()-x).days))\
+            .assign(DAY_OF_YEAR=raw_data.DATE_ENTREE.dt.dayofyear)\
+            .drop(columns=["DATE_ENTREE"])
+
         outliers_pos = self.features["AGE"] > 100
         zeros_pos = self.features["BALANCE"] == 0
-        
+
         self._encode_lands()
-        self.features["SALAIRE"].fillna(self.imput_nan_salaire, inplace = True)
-        self.features["SCORE_CREDIT"].fillna(self.imput_nan_score_credit, inplace = True)
-        self.features["BALANCE"].fillna(self.imput_nan_balance, inplace = True)
+        self.features["SALAIRE"].fillna(self.imput_nan_salaire, inplace=True)
+        self.features["SCORE_CREDIT"].fillna(
+            self.imput_nan_score_credit, inplace=True)
+        self.features["BALANCE"].fillna(self.imput_nan_balance, inplace=True)
         self.features.loc[outliers_pos, "AGE"] = self.imput_outliers_age
         if self.balance_imputation != "none":
             self.features.loc[zeros_pos, "BALANCE"] = self.imput_zero_balance
 
-        
         return self.features
 
-    
-    #ENCODING
+    # ENCODING
+
     def _encode_lands(self):
         """Onehot encoding of 'PAYS' feature."""
         oh_pays = pd.DataFrame(
