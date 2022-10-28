b9e018047cb0e18faa32a300fad8d9bbb6c0d8b8
Emeric COLOMBET <emeric.colombet@2020.icam.fr>
2022-10-28 09:11:46 +0200

    Added save best params to yaml

--- churn/application/main_optimize.py
+++ churn/application/main_optimize.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, os
 from statistics import mode
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -17,10 +17,12 @@ from skopt.space import Real, Categorical, Integer
 from churn.domain.churn_model import ChurnModelSelection
 from churn.domain.domain_utils import get_train_test_split, return_models_from_all_model_params, find_model_params_from_model_name
 from churn.domain.bank_customers_dataset import FeaturesDataset
-from churn.config.config import read_yaml, transform_to_object
+from churn.config.config import read_yaml, transform_to_object, save_best_params_to_yaml
 from churn.domain.domain_utils import get_train_test_split
 from churn.infrastructure.bank_customers import BankCustomersData
+ROOTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 np.random.seed(42)
+#TODO : put it in config.py file
 MODELS_MAPPING_DICT={
     "SVC()" : SVC(),
     "DecisionTreeClassifier()" : DecisionTreeClassifier(),
@@ -30,14 +32,12 @@ MODELS_MAPPING_DICT={
     "ExplainableBoostingClassifier()" : ExplainableBoostingClassifier()
 }
 CONFIG = read_yaml("churn/config/config_template.yml")
-
+# TODO : Mettre ici un argparse avec un chemin par défaut. 
 CONFIG_DATA_INDICATORS = CONFIG["data"]["indicators_dataset"]
 CONFIG_DATA_CUSTOMERS = CONFIG["data"]["customers_dataset"]
-all_models_param = transform_to_object("grid_search_params",MODELS_MAPPING_DICT)
+all_models_param = transform_to_object("churn/config/config_template.yml","grid_search_params",MODELS_MAPPING_DICT)
+
 all_models = return_models_from_all_model_params(all_models_param)
-
-
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--models",
@@ -48,6 +48,10 @@ parser.add_argument("-g", "--GridSearchModel", type=str,
 parser.add_argument("-n", "--n_iter", type=str,
                     help=("(Work only for GridSearchModel) : Number of parameter settings that are sampled. n_iter trades off runtime vs quality of the solution. Default is 5"),
                     default=5)
+
+
+
+
 args = parser.parse_args()
 
 
@@ -60,9 +64,6 @@ feature = FeaturesDataset().transform(raw_data)
 #Waiting for the solution @Charles.
 X_train, X_test, y_train, y_test = train_test_split(feature.drop(columns=["CHURN"]), feature["CHURN"], test_size=0.20, random_state=33)
 #X_train, X_test, y_train, y_test = get_train_test_split()
-
-print(type(all_models_param[0]["pipeline__classifier__gamma"][0]))
-print(all_models_param[0]["pipeline__classifier__gamma"])
 
 #If the user want to visualize the score of each one of the different models. 
 if args.models:
@@ -77,6 +78,7 @@ if args.models:
 #If the user want to commpute a Bayesian Search Optimization for a specific model
 if args.GridSearchModel:
     model_parameters = find_model_params_from_model_name(all_models_param,model_name=args.GridSearchModel)
+    print("model parameters : \n",model_parameters)
     classifier = model_parameters["pipeline__classifier"][0]      
     model = ChurnModelSelection(pipeline=Pipeline([ ('scaler', StandardScaler()),('classifier', classifier)]))
     
@@ -88,6 +90,7 @@ if args.GridSearchModel:
     print("val. score: %s" % opt.best_score_)
     print("test score: %s" % opt.score(X_test, y_test))
     print(f"best params : {opt.best_params_}")
+    save_best_params_to_yaml(path="churn/config/latest_model.yml",best_params=opt.best_params_,model_name=f"{classifier.__class__.__name__}()")
 
 exit()
 
--- churn/config/config.py
+++ churn/config/config.py
@@ -1,26 +1,45 @@
-
-from calendar import c
 from typing import Dict
 import yaml
+import os
+from sklearn.preprocessing import StandardScaler
+from interpret.glassbox import ExplainableBoostingClassifier
 
-def read_yaml(path: str) -> dict:
+def read_yaml(path: os.path) -> dict:
     """Returns YAML file as dict"""
     with open(path, 'r') as file_in:
         config = yaml.unsafe_load(file_in)
     return config
 
 
-    return 
-def transform_to_object(path_of_models : str ,mapping_dict : Dict):
+def transform_to_object(file_path : os.path, path_of_models : str ,mapping_dict : Dict):
     """
     This function transform models name from string to class. 
     Ex : from "SVC()" to sklearn.SVC() """
 
-    config = read_yaml("churn/config/config_template.yml")
-    grid_parameters = config["model"][str(path_of_models)]
+    config = read_yaml(file_path)
+    if path_of_models is not None:
+        grid_parameters = config[str(path_of_models)]
+    else :
+        grid_parameters = config
     model_list = list()
     for model in grid_parameters :
+        print(f"Model pipeline classifier : {grid_parameters}")
         model["pipeline__classifier"][0] = mapping_dict[model["pipeline__classifier"][0]]
         model_list.append(model)
     return model_list
 
+def save_best_params_to_yaml (path : str ,best_params : tuple,model_name : str):
+    """This function save the result of a Bayesian search into a special file used by the train model. """
+    model_to_save = dict()
+    model_to_save["pipeline__classifier"] = [model_name]
+    print(f"best_params {best_params}")
+    for param in best_params:
+        # Skiping the result of pipeline__classifier, because this object coming from Bayesian search hasn't the right format. 
+        if param != "pipeline__classifier":
+            model_to_save[param] = best_params[param]
+    model_to_save = {
+        "model_parameters" : [model_to_save]
+    }
+    with open(path, 'w') as file:
+        yaml.dump(model_to_save, file)
+    return True
