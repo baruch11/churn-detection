1501af88e2d6bd3b95842e9310067e73e15184fd
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2022-12-14 16:18:00 +0100

    add setup.py at root of the directory

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='churn_sec',
+    url='https://gitlab.com/yotta-academy/mle-bootcamp/projects/ml-project/project-1-fall-2022/churn-modelling-salima-charles-emeric.git',
+    author='Salima, Emeric, Charles (Coyotta)',
+    description='Projet1: Churn detection',
+    packages=find_packages()
+)
